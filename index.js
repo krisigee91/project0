1d05db37d21484009939d3b8432ce40e89e1f6ac
krisigee91 <30305526+krisigee91@users.noreply.github.com>
2017-08-21 09:56:06 -0500

    original files

---
+++ index.js
@@ -0,0 +1,29 @@
+
+$(document).ready ( function() {
+
+
+    $('#orb').click(function() {
+        
+            
+        if ($('#orb').hasClass('sun')) {
+     
+            $('#orb').removeClass('sun').addClass('moon');
+        } else {
+            
+            $('#orb').removeClass('moon').addClass('sun');}
+            
+      
+      
+        if ($('#sky').hasClass('day')) {
+     
+            $('#sky').removeClass('day').addClass('night');
+        } else {
+            
+            $('#sky').removeClass('night').addClass('day');}
+    
+    
+});
+ 
+
+
+});
